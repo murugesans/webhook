1735a4bb9c51705a42db4538241e15349c8654d2
murugesans <30517176+murugesans@users.noreply.github.com>
2019-07-17 14:43:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
 		    file_put_contents('disputes_logs'.date("Y-m-d").'.txt', $post.PHP_EOL , FILE_APPEND | LOCK_EX);
 		}elseif($array->event_type == 'BILLING_AGREEMENTS.AGREEMENT.CREATED' || $array->event_type == 'BILLING_AGREEMENTS.AGREEMENT.CANCELLED' || $array->event_type == 'BILLING_AGREEMENTS.AGREEMENT.CANCEL_PENDING'){
 		    file_put_contents('BA'.date("Y-m-d").'.txt', $post.PHP_EOL , FILE_APPEND | LOCK_EX);
+		}elseif($array->event_type == 'PAYMENT.ORDER.CREATED' || $array->event_type == 'PAYMENT.ORDER.CANCELLED' || $array->event_type == 'PAYMENTS.PAYMENT.CREATED' ||$array->event_type == 'PAYMENT.AUTHORIZATION.CREATED' || $array->event_type == 'PAYMENT.AUTHORIZATION.VOIDED') {
+	 		file_put_contents('payment_logs'.date("Y-m-d").'.txt', $post.PHP_EOL , FILE_APPEND | LOCK_EX);
+	 		file_put_contents('payment_headerlogs'.date("Y-m-d").'.txt', $_SERVER , FILE_APPEND | LOCK_EX);
+		}elseif($array->event_type == 'PAYMENT.CAPTURE.COMPLETED' || $array->event_type == 'PAYMENT.CAPTURE.DENIED'|| $array->event_type == 'PAYMENT.CAPTURE.PENDING'|| $array->event_type == 'PAYMENT.CAPTURE.REFUNDED'|| $array->event_type == 'PAYMENT.CAPTURE.REVERSED') {
+	 		file_put_contents('CAPTURE_logs'.date("Y-m-d").'.txt', $post.PHP_EOL , FILE_APPEND | LOCK_EX);
+	 		file_put_contents('CAPTURE_headerlogs'.date("Y-m-d").'.txt', $_SERVER , FILE_APPEND | LOCK_EX);
+		}elseif($array->event_type == 'CHECKOUT.ORDER.APPROVED') {
+	 		file_put_contents('order_logs'.date("Y-m-d").'.txt', $post.PHP_EOL , FILE_APPEND | LOCK_EX);
+	 		file_put_contents('order_headerlogs'.date("Y-m-d").'.txt', $_SERVER , FILE_APPEND | LOCK_EX);
 		}
     } else {
     	echo 'Send POST.';
